5f2a1951cc96ce1c7e3f43a319f2144a0fefa17f
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2024-11-14 22:57:48 +0800

    Update URL Factory

--- database/factories/UrlFactory.php
+++ database/factories/UrlFactory.php
@@ -4,6 +4,8 @@ namespace CleaniqueCoders\Shrinkr\Database\Factories;
 
 use CleaniqueCoders\Shrinkr\Models\Url;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+use Workbench\App\Models\User;
 
 class UrlFactory extends Factory
 {
@@ -12,6 +14,8 @@ class UrlFactory extends Factory
     public function definition()
     {
         return [
+            'uuid' => Str::uuid(),
+            'user_id' => User::factory(),
             'original_url' => fake()->url(),
             'shortened_url' => $this->generateShortCode(),
             'custom_slug' => null,
